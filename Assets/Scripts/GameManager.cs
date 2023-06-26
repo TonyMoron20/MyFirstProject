1a0fafecc783a2337887e704dde499601fa5e108
Álvaro Antonio Morón <tonymoron20@gmail.com>
2023-06-26 17:25:45 -0600

    Pausa y repetición de nivel
    Se agregan funcionalidades al GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public string labelText = "Recolecta todos los items";
     public int maxItems = 2;
+    public bool showWinScreen = false;
 
     private int _itemsCollected = 0;
     public int Items
@@ -13,13 +15,16 @@ public class GameManager : MonoBehaviour
         get { return _itemsCollected; }
         set
         {
+            _itemsCollected = value;
             if(_itemsCollected >= maxItems)
             {
                 labelText = "Has encontrado todos los items";
+                showWinScreen = true;
+                // Congela todo el juego, es poner en pausa el juego.
+                Time.timeScale = 0.0f;
             }
             else
             {
-                _itemsCollected = value;
                 labelText = "item encontrado, te faltan: " + (maxItems - _itemsCollected);
             }
         }
@@ -41,6 +46,17 @@ public class GameManager : MonoBehaviour
         GUI.Box(new Rect(25, 65, 180, 25), "Items: " + _itemsCollected);
 
         GUI.Label(new Rect(Screen.width / 2.0f - 100, Screen.height - 50, 200, 50), labelText);
+
+        if(showWinScreen)
+        {
+            // Si se pulsa el boton
+            if (GUI.Button(new Rect(Screen.width / 2.0f - 200, Screen.height / 2.0f - 100, 400, 200), "¡¡HAS GANADO!!"))
+            {
+                // Carga nuevamente la escena
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
+            }
+        }
     }
 
 }
